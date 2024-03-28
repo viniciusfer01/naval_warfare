d81ba682a9415d638834fb9ef563917d56168a51
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-03-28 16:48:55 -0300

    Cells now are collored

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import "./App.css";
 import Table from "./table";
+import { useState } from "react";
 
 function App() {
+  const [isYourTurn, setIsYourTurn] = useState(true);
+
   return (
     <>
       <h1>Batalha Naval</h1>
       <div className="card">
-        <Table team={"yours"} />
-        <Table team={"adversary"} />
+        <Table team={"yours"} isYourTurn={isYourTurn} />
+        <Table team={"adversary"} isYourTurn={isYourTurn} />
       </div>
-      <p className="read-the-docs"></p>
+      <div className="ship-selection">
+        <p>1 Porta-aviões</p>
+        <p>2 Encouraçados</p>
+        <p>3 Hidroaviões</p>
+        <p>4 Submarinos</p>
+        <p>3 Cruzadores</p>
+      </div>
     </>
   );
 }
--- src/table.jsx
+++ src/table.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const TABLE_SIZE = 11;
 const rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
 
@@ -13,11 +15,39 @@ function tableElementDecider(i, j) {
   }
 }
 
-function buttonClickHandler(team, i, j) {
-  console.log(team, i, j);
+function buttonClickHandler(
+  team,
+  i,
+  j,
+  isYourTurn,
+  setClickedCells,
+  clickedButtons
+) {
+  if (i === 0 || j === 0) {
+    return;
+  } else if (isYourTurn) {
+    if (team === "yours") {
+      console.log(`You clicked on your cell ${rows[i - 1]}${j}`);
+      // check if the cell was already clicked
+      if (clickedButtons.some((button) => button.i === i && button.j === j)) {
+        // delete the cell from the clickedCells array
+        setClickedCells((prevButtons) =>
+          prevButtons.filter((button) => button.i !== i || button.j !== j)
+        );
+      } else {
+        setClickedCells((prevButtons) => [...prevButtons, { i, j }]);
+      }
+    } else {
+      console.log(`You clicked on the enemy's cell ${rows[i - 1]}${j}`);
+    }
+  } else {
+    return;
+  }
 }
 
 function Table(props) {
+  const [clickedButtons, setClickedCells] = useState([]);
+
   return (
     <div className="table">
       {Array.from({ length: TABLE_SIZE }).map((_, i) => (
@@ -26,8 +56,22 @@ function Table(props) {
             <div key={j} className="cell">
               <button
                 className="cell-button"
+                style={{
+                  backgroundColor: clickedButtons.some(
+                    (button) => button.i === i && button.j === j
+                  )
+                    ? "red"
+                    : "#1a1a1a",
+                }}
                 onClick={() => {
-                  buttonClickHandler(props.team, i, j);
+                  buttonClickHandler(
+                    props.team,
+                    i,
+                    j,
+                    props.isYourTurn,
+                    setClickedCells,
+                    clickedButtons
+                  );
                 }}
               >
                 {tableElementDecider(i, j)}
