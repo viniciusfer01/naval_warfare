ce994b7df17c8e92ad2f4326b0ff84663d0b75fa
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-04-01 13:49:22 -0300

    added waiting state and client-server connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,47 @@
 import "./App.css";
 import Table from "./table";
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 function App() {
-  const [isYourTurn, setIsYourTurn] = useState(true);
+  const [isYourTurn, setIsYourTurn] = useState(false);
+  const [isWaiting, setIsWaiting] = useState(true);
+  const socket = useMemo(() => new WebSocket("ws://localhost:8080"), []);
+
+  useEffect(() => {
+    socket.addEventListener("open", (event) => {
+      socket.send("Connection established!");
+    });
+
+    socket.addEventListener("message", (event) => {
+      console.log("Message from server: ", event.data);
+    });
+
+    socket.addEventListener("close", (event) => {
+      console.log("Connection closed!");
+      setIsWaiting(true);
+    });
+  }, [socket]);
 
   return (
     <>
       <h1>Batalha Naval</h1>
-      <div className="card">
-        <Table team={"yours"} isYourTurn={isYourTurn} />
-        <Table team={"adversary"} isYourTurn={isYourTurn} />
-      </div>
-      <div className="ship-selection">
-        <p>1 Porta-aviões</p>
-        <p>2 Encouraçados</p>
-        <p>3 Hidroaviões</p>
-        <p>4 Submarinos</p>
-        <p>3 Cruzadores</p>
-      </div>
+      {isWaiting ? (
+        <p>Waiting for other players...</p>
+      ) : (
+        <>
+          <div className="card">
+            <Table team={"yours"} isYourTurn={isYourTurn} />
+            <Table team={"adversary"} isYourTurn={isYourTurn} />
+          </div>
+          <div className="ship-selection">
+            <p>1 Porta-aviões</p>
+            <p>2 Encouraçados</p>
+            <p>3 Hidroaviões</p>
+            <p>4 Submarinos</p>
+            <p>3 Cruzadores</p>
+          </div>
+        </>
+      )}
     </>
   );
 }
